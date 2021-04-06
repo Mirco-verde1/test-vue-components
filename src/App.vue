8a36433815a9514fbb493cf63e0b6862ebae52f8
Mirco Verderosa <verderosamircowork@gmail.com>
2021-04-06 22:28:34 +0200

    add little title component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
+    <List title = "Prova componente vue"/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    List
   }
 }
 </script>
---
+++ src/components/List.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    name: 'List',
+    props: {
+        title:String
+    }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
